0514304170aea2481ee76e9be7d7caae0b073c98
Igor A <aiv_igor@yahoo.com>
2021-11-28 19:32:18 +0200

    Added test file

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,44 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("KIMarket", function () {
+  it("Should mint anr trade NFTs", async function () {
+    const Market = await ethers.getContractFactory('KIMarket');
+    const market = await Market.deploy();
+    await market.deployed();
+    const marketAddress = market.address;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory('NFT');
+    const nft = await NFT.deploy(marketAddress);
+    await nft.deployed();
+    const nftContractAddress = nft.address;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parserUnits('100', 'ether');
+    
+    //test for minting
+    await nft.mintToken('https-t1');
+    await nft.mintToken('https-t2');
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await market.makeMarketItem(nftContractAddress, 1, auctionPrice, {value: listingPrice});
+    await market.makeMarketItem(nftContractAddress, 2, auctionPrice, {value: listingPrice});
+
+    // test for different addresses from different users - test accounts
+    // return an array of however many addresses
+
+    const [_, buyerAddress] = await ethers.getSigners();
+
+    // create a market sale with address, id and price 
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, {
+      value: auctionPrice
+    });
+
+    const items = await market.fetchMarketTokens();
+
+    // test uot all the items 
+    console.log("items", items);
   });
 });
