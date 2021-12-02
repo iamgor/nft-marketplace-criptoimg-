c6e528c757c8008b9e7fe0f1c6188e0a2b305885
Igor A <aiv_igor@yahoo.com>
2021-12-02 11:50:05 +0200

    Modified files & added new functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import './app.css'
+import Link from "next/link";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function CryptoImgMarketplace({Component, pageProps}) {
+  return (
+    <div>
+      <nav className = "border-b p-6" style={{backgroundColor:'purple'}}>
+        <p className = 'text-4x1 font-bold text-white'>Crypto Image Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'>
+              Maine Marketplace
+              </a>
+          </Link>
+          <Link href='/mint-item'>
+            <a className='mr-6'>
+              Mint Tokens 
+              </a>
+          </Link>
+          <Link href='/my-nfts'>
+            <a className='mr-6'>
+              Maine Marketplace
+              </a>
+          </Link>
+          <Link href='/account-dashboard'>
+            <a className='mr-6'>
+              Account Dashboard
+              </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps}/>
+    </div>
+  )
 }
-
-export default MyApp
+export default CryptoImgMarketplace
